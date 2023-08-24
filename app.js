bc591e5000747994c2d60190845999d814a518a0
Vishwajeet Salunke <salunkevishwajeet577@gmail.com>
2023-08-24 18:15:45 +0530

    implemented lvl4 encryption using bcrypt and salting

--- app.js
+++ app.js
@@ -1,78 +1,78 @@
-
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import env from "dotenv";
+import bcrypt from "bcrypt";
 // import encrypt from 'mongoose-encryption';
-import env from 'dotenv';
-import md5 from 'md5';
+// import md5 from 'md5';
 
-env.config()
+env.config();
 
 const app = express();
-const port = process.env.PORT || 3000
-app.set('view engine','ejs')
-app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended:true}))
+const port = process.env.PORT || 3000;
 
-mongoose.connect("mongodb://127.0.0.1:27017/UserDB")
+const saltRounds = 10;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://127.0.0.1:27017/UserDB");
 const userSchema = new mongoose.Schema({
-    email:String,
-    password:String
-})
+  email: String,
+  password: String,
+});
 
 // userSchema.plugin(encrypt,{secret:process.env.KEY,encryptedFields:["password"]})
 
-const User = mongoose.model("user",userSchema)
+const User = mongoose.model("user", userSchema);
 
-app.get("/",(req,res)=>{
-    res.render("home");
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get("/register",(req,res)=>{
-    res.render("register");
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-app.get("/login",(req,res)=>{
-    res.render("login");
-})
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.get("/logout",(req,res)=>{
-    res.redirect("/login");
-})
+app.get("/logout", (req, res) => {
+  res.redirect("/login");
+});
 
-app.post("/login",(req,res)=>{
-    async function login(){
-        const user = await User.findOne({email:req.body.username});
-        if(user){
-            if(user.email ===  req.body.username && user.password === md5(req.body.password)){
-                console.log(md5(req.body.password));
-                res.render("secrets")
-            }else{
-                res.redirect("login")
-            }
-        }
+app.post("/login", (req, res) => {
+  async function login() {
+    const user = await User.findOne({ email: req.body.username });
+    if (user) {
+      const match = await bcrypt.compare(req.body.password, user.password);
+      if (user.email === req.body.username && match) {
+        
+        res.render("secrets");
+      } else {
+        res.redirect("login");
+      }
     }
-    login()
-    
-})
+  }
+  login();
+});
 
-app.post("/register",(req,res)=>{
-    try {
-        async function register(){
-            const user = new User({
-                email:req.body.username,
-                password:md5(req.body.password)
-            })
-            user.save()
-            res.render("login")
-        }    
-        register()    
-    } catch (error) {
-        console.log(error);
-        res.redirect("home")
-    } 
-})
+app.post("/register", (req, res) => {
+  async function register() {
+    bcrypt.hash(req.body.password, saltRounds).then((result) => {
+      const user = new User({
+        email: req.body.username,
+        password: result,
+      });
+      user.save();
+    });
+    res.render("login");
+  }
+  register();
+});
 
-app.listen(port,()=>{
-    console.log("listening on port 3000");
-})
+app.listen(port, () => {
+  console.log("listening on port 3000");
+});
